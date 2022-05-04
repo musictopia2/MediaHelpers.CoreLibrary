db1b05210b3bb4f88793fc6437dcd4d81bdd0d06
Andy Walter <musictopia2@hotmail.com>
2022-05-04 08:38:36 -0700

    Added support to delete song and do weight stuff via data access.  I propose possibly creating a new class to address this.

---
+++ Music/RemoteControls/IMusicRemoteControlDataAccess.cs
@@ -0,0 +1,8 @@
+﻿namespace MediaHelpers.CoreLibrary.Music.RemoteControls;
+public interface IMusicRemoteControlDataAccess
+{
+    Task IncreaseWeightAsync(IBaseSong song);
+    Task DecreaseWeightAsync(IBaseSong song);
+    int GetWeight(IBaseSong song);
+    Task DeleteSongAsync(IBaseSong song);
+}
