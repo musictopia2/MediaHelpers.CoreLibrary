a5bc0209b909cc2427690becacc78a6a52d658be
Andy Walter <musictopia2@hotmail.com>
2024-01-10 09:09:14 -0700

    Updated show table interface. Some properties are not even necessary here.

--- Video/DatabaseInterfaces/TVShows/IShowTable.cs
+++ Video/DatabaseInterfaces/TVShows/IShowTable.cs
@@ -3,9 +3,6 @@ public interface IShowTable
 {
     int ID { get; set; }
     string ShowName { get; set; }
-    int? OpeningLength { get; set; }
-    int? ClosingLength { get; set; }
     bool FinishedFirstRun { get; set; }
-    bool AlwaysSkipOpening { get; set; }
     EnumTelevisionLengthType LengthType { get; set; }
 }
