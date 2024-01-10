923d1da9c73e11ae75410af3e4ac5e90b0e436fd
Andy Walter <musictopia2@hotmail.com>
2024-01-10 16:49:48 -0700

    Code cleanup.

--- Video/DatabaseInterfaces/TVShows/IRerunLoaderTelevisionContext.cs
+++ Video/DatabaseInterfaces/TVShows/IRerunLoaderTelevisionContext.cs
@@ -1,9 +1,7 @@
 ﻿namespace MediaHelpers.CoreLibrary.Video.DatabaseInterfaces.TVShows;
 public interface IRerunLoaderTelevisionContext : IStartLoaderTelevisionContext
 {
-
-    //Task<BasicList<IEpisodeTable>> GetHolidayListAsync(EnumTelevisionHoliday currentHoliday);
-    Task<IEpisodeTable> GetManuelEpisodeAsync(int showID, int episodeID); //reruns should not need this.
+    Task<IEpisodeTable> GetManuelEpisodeAsync(int showID, int episodeID);
     Task TemporarilySkipEpisodeAsync();
     Task EndEpisodeAsync(); //if episode is needed, should be under CurrentEpisode.
     Task<bool> CanAutomaticallyGoToNextEpisodeAsync();
