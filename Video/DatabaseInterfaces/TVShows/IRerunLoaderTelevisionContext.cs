11cafc718a6ff96c1cef5717d454b797984541de
Andy Walter <musictopia2@hotmail.com>
2024-01-13 11:43:15 -0700

    Updated to return the real episode and not interface.

--- Video/DatabaseInterfaces/TVShows/IRerunLoaderTelevisionContext.cs
+++ Video/DatabaseInterfaces/TVShows/IRerunLoaderTelevisionContext.cs
@@ -2,7 +2,7 @@
 public interface IRerunLoaderTelevisionContext<E> : IStartLoaderTelevisionContext<E>, IRerunStartTelevisionContext<E>
     where E : class, IEpisodeTable
 {
-    Task<IEpisodeTable> GetManuelEpisodeAsync(int showID, int episodeID);
+    Task<E> GetManuelEpisodeAsync(int showID, int episodeID);
     Task TemporarilySkipEpisodeAsync();
     Task EndEpisodeAsync(); //if episode is needed, should be under CurrentEpisode.
     Task<bool> CanAutomaticallyGoToNextEpisodeAsync();
