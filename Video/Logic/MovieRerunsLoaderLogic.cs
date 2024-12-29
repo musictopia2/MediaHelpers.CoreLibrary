0ed711249c5eab0f226d782f7caf242e009d7b66
Andy Walter <musictopia2@hotmail.com>
2024-12-28 18:05:46 -0700

    If marking as disliking movie or needing to edit later, should close out.

--- Video/Logic/MovieRerunsLoaderLogic.cs
+++ Video/Logic/MovieRerunsLoaderLogic.cs
@@ -2,24 +2,26 @@
 public class MovieRerunsLoaderLogic<M>(IRerunLoaderMovieContext<M> dats, IExit exit) : IRerunMovieLoaderLogic<M>
     where M : class, IMainMovieTable
 {
-    Task IMovieLoaderLogic<M>.DislikeMovieAsync(M selectedMovie)
+    async Task IMovieLoaderLogic<M>.DislikeMovieAsync(M selectedMovie)
     {
         dats.CurrentMovie = selectedMovie;
-        return dats.DislikeMovieAsync();
+        await dats.DislikeMovieAsync();
+        exit.ExitApp();
     }
-    Task IRerunMovieLoaderLogic<M>.EditMovieLaterAsync(M selectedMovie)
+    async Task IRerunMovieLoaderLogic<M>.EditMovieLaterAsync(M selectedMovie)
     {
         dats.CurrentMovie = selectedMovie;
-        return dats.EditMovieLaterAsync();
+        await dats.EditMovieLaterAsync();
+        exit.ExitApp();
     }
     async Task IMovieLoaderLogic<M>.FinishMovieAsync(M selectedMovie)
     {
-        selectedMovie.ResumeAt = null;
         await UpdateMovieAsync(selectedMovie);
         exit.ExitApp();
     }
     private Task UpdateMovieAsync(M selectedMovie)
     {
+        dats.CurrentMovie = selectedMovie;
         selectedMovie.ResumeAt = null;
         return dats.UpdateMovieAsync();
     }
